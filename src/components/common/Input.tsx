2f01887e929e3fdd9585995dd272ea0a3ef92d6c
Github <khmin1106@gmail.com>
2025-07-15 14:45:09 +0900

    ✨ feat: input 컴포넌트 ( 이미지 삽입 전)

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,3 +1,17 @@
 export default function Input() {
-  return <div>인풋</div>;
+  return (
+    <div className="flex items-center lg:w-[285px] lg:h-[40px] border border-dark-green rounded-3xl overflow-hidden">
+      <input
+        type="text"
+        placeholder="상품명을 입력해주세요"
+        className="flex-grow px-3  text-sm outline-none"
+      />
+      <button
+        type="submit"
+        className="w-10 h-full flex justify-center items-center hover:bg-gray-300"
+      >
+        <svg></svg>
+      </button>
+    </div>
+  );
 }
