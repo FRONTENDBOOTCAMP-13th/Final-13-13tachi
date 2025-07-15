44bf0e742c3b33a710a165536ed07f96505fd966
Github <khmin1106@gmail.com>
2025-07-15 14:44:35 +0900

    ✨ feat: 버튼 컴포넌트

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,3 +1,36 @@
-export default function Button() {
-  return <div>버튼</div>;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'green' | 'white';
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+}
+
+export default function Button({
+  children,
+  variant = 'green',
+  size = 'xl',
+  ...rest
+}: ButtonProps) {
+  const baseStyle = 'font-semibold rounded-lg shadow-[var(--btn-shadow)]';
+
+  const variantStyle = {
+    green: 'bg-dark-green text-white',
+    white: 'bg-disable-gray text-black border border-light-gray',
+  };
+
+  const sizeStyle = {
+    xs: 'lg:w-[65px] lg:h-[30px] lg:text-sm',
+    sm: 'lg:w-[80px] lg:h-[30px] lg:text-sm',
+    md: 'lg:w-[100px] lg:h-[40px] lg:text-base',
+    lg: 'lg:w-[120px] lg:h-[30px] lg:text-sm',
+    xl: 'lg:w-[180px] lg:h-[50px] lg:text-base',
+    xxl: 'lg:w-[200px] lg:h-[48px] lg:text-base',
+  };
+
+  return (
+    <button
+      className={`${baseStyle} ${variantStyle[variant]} ${sizeStyle[size]}`}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }
