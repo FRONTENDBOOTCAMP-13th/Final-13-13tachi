d8082d30c30fce72014b59173f4d1bc34ca5fd7e
Github <khmin1106@gmail.com>
2025-07-15 21:35:37 +0900

    ✨ feat: input 컴포넌트 제작

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,19 +1,29 @@
-import Image from 'next/image';
+import React from 'react';
 
-export default function Input() {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  width?: 'lg' | 'md' | 'sm' | 'xs';
+  className?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function Input({
+  width = 'md',
+  className = '',
+  onChange,
+  ...rest
+}: InputProps) {
+  const widthClasses = {
+    lg: 'w-[780px]',
+    md: 'w-[330px]',
+    sm: 'w-[175px]',
+    xs: 'w-[119px]',
+  };
+
   return (
-    <div className="flex items-center lg:w-[285px] lg:h-[40px] border border-dark-green rounded-3xl overflow-hidden">
-      <input
-        type="text"
-        placeholder="상품명을 입력해주세요"
-        className="flex-grow px-3  text-sm outline-none"
-      />
-      <button
-        type="submit"
-        className="w-10 h-full flex justify-center items-center"
-      >
-        <Image src="/input.svg" alt="검색" width={16} height={16} />
-      </button>
-    </div>
+    <input
+      className={`h-[45px] ${widthClasses[width]} px-3 text-sm border border-light-gray rounded-lg text-black placeholder-gray-400 focus:outline-gray  ${className}`}
+      onChange={onChange}
+      {...rest}
+    />
   );
 }
