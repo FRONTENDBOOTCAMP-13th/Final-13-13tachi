b04b2b2c6b8e4b89f6b738cce72181e0eeeece73
Github <khmin1106@gmail.com>
2025-07-15 14:56:04 +0900

    ✨ feat: input 컴포넌트

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function Input() {
   return (
     <div className="flex items-center lg:w-[285px] lg:h-[40px] border border-dark-green rounded-3xl overflow-hidden">
@@ -8,9 +10,9 @@ export default function Input() {
       />
       <button
         type="submit"
-        className="w-10 h-full flex justify-center items-center hover:bg-gray-300"
+        className="w-10 h-full flex justify-center items-center"
       >
-        <svg></svg>
+        <Image src="/input.svg" alt="검색" width={16} height={16} />
       </button>
     </div>
   );
