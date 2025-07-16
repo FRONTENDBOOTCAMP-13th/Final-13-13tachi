70a7ab256db3aab6fbd72d41936e0bedcfa57770
imnotpossib1e <habit2me@gmail.com>
2025-07-16 13:08:39 +0900

    💄 style: 버튼 컴포넌트 사이즈 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,6 +1,6 @@
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'green' | 'white';
-  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+  size?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxlsm';
 }
 
 export default function Button({
@@ -17,12 +17,14 @@ export default function Button({
   };
 
   const sizeStyle = {
+    xxs: 'lg:w-[97px] lg:h-[25px] lg:text-xs',
     xs: 'lg:w-[65px] lg:h-[30px] lg:text-sm',
     sm: 'lg:w-[80px] lg:h-[30px] lg:text-sm',
     md: 'lg:w-[100px] lg:h-[40px] lg:text-base',
     lg: 'lg:w-[120px] lg:h-[30px] lg:text-sm',
     xl: 'lg:w-[180px] lg:h-[50px] lg:text-base',
     xxl: 'lg:w-[200px] lg:h-[48px] lg:text-base',
+    xxlsm: 'lg:w-[100px] lg:h-[48px] lg:text-base',
   };
 
   return (
