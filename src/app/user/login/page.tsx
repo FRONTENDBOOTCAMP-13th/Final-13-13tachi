5c97cf2bdca29c10aef6014cc5f060e501598e82
Github <khmin1106@gmail.com>
2025-07-16 11:28:38 +0900

    💄 styles: lg: 정리

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -12,48 +12,48 @@ export default async function LoginPage() {
       />
       <div className="absolute inset-0 -z-10 bg-black opacity-50" />
 
-      <main className="py-[5.9375rem] flex justify-center items-center min-h-full">
+      <main className="lg:py-[5.9375rem] flex justify-center items-center min-h-full">
         <div
-          className="py-[4.3125rem] rounded-[0.9375rem] w-[25rem] h-[31.25rem] max-w-md bg-white"
+          className="lg:py-[4.3125rem] rounded-[0.9375rem] lg:w-[25rem] lg:h-[31.25rem] max-w-md bg-white"
           style={{ boxShadow: '.375rem .375rem .25rem rgba(0, 0, 0, 0.25)' }}
         >
           <div className="flex flex-col items-center text-center ">
             <Image src="/logo-black.svg" alt="로고" width={110} height={70} />
-            <h2 className="text-4xl font-semibold mt-[0.625rem] text-dark-green">
+            <h2 className="lg:text-4xl lg:font-semibold lg:mt-[0.625rem] text-dark-green">
               로그인
             </h2>
           </div>
 
           <form action="/">
-            <div className="mb-[0.625rem] mt-[1.25rem] flex justify-center ">
+            <div className="lg:mb-[0.625rem] lg:mt-[1.25rem] flex justify-center ">
               <input
                 id="email"
                 type="email"
                 autoComplete="email"
                 placeholder="이메일을 입력하세요"
-                className="w-[18.125rem] h-[2.8125rem] px-[0.75rem] py-[0.5rem] border border-light-gray rounded-lg focus:outline-none focus:border-gray "
+                className="lg:w-[18.125rem] lg:h-[2.8125rem] lg:px-[0.75rem] lg:py-[0.5rem] border border-light-gray rounded-lg focus:outline-none focus:border-gray "
                 name="email"
               />
             </div>
-            <div className="mb-[0.625rem] flex justify-center">
+            <div className="lg:mb-[0.625rem] flex justify-center">
               <input
                 id="password"
                 type="password"
                 autoComplete="current-password"
                 placeholder="비밀번호를 입력하세요"
-                className="w-[18.125rem] h-[2.8125rem] px-[0.75rem] py-[0.5rem] border border-light-gray rounded-lg focus:outline-none focus:border-gray"
+                className="lg:w-[18.125rem] lg:h-[2.8125rem] lg:px-[0.75rem] lg:py-[0.5rem] border border-light-gray rounded-lg focus:outline-none focus:border-gray"
                 name="password"
               />
             </div>
-            <div className="flex justify-between items-center w-[18.125rem] mx-auto ml-auto text-gray text-xs hover:underline">
+            <div className="flex justify-between items-center lg:w-[18.125rem] mx-auto ml-auto text-gray text-xs hover:underline">
               <div className="flex items-center gap-1">
-                <Checkbox className="w-[1.25rem] lg:h-[1.25rem]" />
+                <Checkbox className="lg:w-[1.25rem] lg:h-[1.25rem]" />
                 <p>자동 로그인</p>
               </div>
               <Link href="#">회원가입</Link>
             </div>
 
-            <div className="flex justify-center items-center mt-[3.125rem]">
+            <div className="flex justify-center items-center lg:mt-[3.125rem]">
               <Button size="md">로그인</Button>
             </div>
           </form>
--- src/app/user/signup/page.tsx
+++ src/app/user/signup/page.tsx
@@ -1,120 +1,141 @@
 import Button from '@/components/common/Button';
+import Input from '@/components/common/Input';
 
 export default async function SignupPage() {
   return (
     <main className="flex-grow flex flex-col items-center justify-center">
       <div className="flex flex-col items-center mb-6">
-        <h2 className="text-5xl font-bold text-black mt-[70px] mb-2">
+        <h2 className="lg:text-5xl font-bold text-black lg:mt-[4.375rem] lg:mb-2">
           회원 가입
         </h2>
-        <div className="w-[1024px] flex flex-col items-end">
-          <div className="flex mt-[12px]">
+        <div className="lg:w-[64rem] flex flex-col items-end">
+          <div className="flex lg:mt-[0.75rem]">
             <p className="text-dark-red text-sm">*</p>
-            <p className=" text-sm text-gray">는 필수 입력</p>
+            <p className="lg:text-sm text-gray">는 필수 입력</p>
           </div>
-          <hr className="w-[1024px] h-px md-[30px] bg-light-gray border-0" />
+          <hr className="lg:w-[64rem] h-px lg:md-[1.875rem] bg-light-gray border-0" />
         </div>
       </div>
 
-      <form action="/" className="w-[458px]">
-        <div className="flex items-center justify-between mb-[10px]">
+      <form action="/" className="lg:w-[28.625rem]">
+        <div className="flex items-center justify-between lg:mb-[0.625rem]">
           <div className="flex items-center">
-            <label className="block text-black text-base" htmlFor="email">
+            <label className="block text-black lg:text-base" htmlFor="email">
               이메일
             </label>
-            <span className="text-dark-red text-sm ml-1">*</span>
+            <span className="text-dark-red lg:text-sm lg:ml-1">*</span>
           </div>
-          <input
+          <Input
+            width="md"
             type="email"
             id="email"
-            autoComplete="username"
+            autoComplete="email"
             placeholder="이메일을 입력하세요"
-            className="w-[330px] px-3 py-2 text-base border rounded-lg focus:outline-none"
             name="email"
           />
         </div>
-        <div className="flex items-center justify-between mb-[10px]">
+        <div className="flex items-center justify-between lg:mb-[0.625rem]">
           <div className="flex items-center">
-            <label className="block text-black text-base" htmlFor="password">
+            <label className="block text-black lg:text-base" htmlFor="password">
               비밀번호
             </label>
-            <span className="text-dark-red text-sm ml-1">*</span>
+            <span className="text-dark-red lg:text-sm lg:ml-1">*</span>
           </div>
-          <input
+          <Input
+            width="md"
             type="password"
             id="password"
             autoComplete="new-password"
             placeholder="비밀번호를 입력하세요"
-            className="w-[330px] px-3 py-2 text-base border rounded-lg focus:outline-none"
             name="password"
           />
         </div>
-        <div className="flex items-center justify-between mb-[10px]">
+        <div className="flex items-center justify-between lg:mb-[0.625rem]">
           <div className="flex items-center">
-            <label className="block text-black text-base" htmlFor="password">
+            <label className="block text-black lg:text-base" htmlFor="password">
               비밀번호 확인
             </label>
-            <span className="text-dark-red text-sm ml-1">*</span>
+            <span className="text-dark-red lg:text-sm lg:ml-1">*</span>
           </div>
-          <input
+          <Input
+            width="md"
             type="password"
             id="password"
             autoComplete="new-password"
-            placeholder="비밀번호를 입력하세요"
-            className="w-[330px] px-3 py-2 text-base border rounded-lg focus:outline-none"
+            placeholder="비밀번호를 한번 더 입력하세요"
             name="password"
           />
         </div>
-        <div className="flex items-center justify-between mb-[10px]">
+        <div className="flex items-center justify-between lg:mb-[0.625rem]">
           <div className="flex items-center">
-            <label className="block text-black text-base" htmlFor="name">
+            <label className="block text-black lg:text-base" htmlFor="name">
               이름
             </label>
-            <span className="text-dark-red text-sm ml-1">*</span>
+            <span className="text-dark-red lg:text-sm lg:ml-1">*</span>
           </div>
-          <input
+          <Input
+            width="md"
             type="text"
             id="name"
             autoComplete="name"
             placeholder="이름을 입력하세요"
-            className="w-[330px] px-3 py-2 text-base border rounded-lg focus:outline-none "
             name="name"
           />
         </div>
-        <div className="flex items-center justify-between mb-[10px]">
+        <div className="flex items-center justify-between lg:mb-[0.625rem]">
           <div className="flex items-center">
-            <label className="block text-black text-base" htmlFor="phone">
+            <label className="block text-black lg:text-base" htmlFor="phone">
               전화번호
             </label>
-            <span className="text-dark-red text-sm ml-1">*</span>
+            <span className="text-dark-red lg:text-sm lg:ml-1">*</span>
           </div>
-          <input
+          <Input
+            width="md"
             type="text"
-            id="name"
-            autoComplete="name"
-            placeholder="이름을 입력하세요"
-            className="w-[330px] px-3 py-2 text-base border rounded-lg focus:outline-none "
-            name="name"
+            id="phone"
+            autoComplete="tel"
+            placeholder="전화번호를 입력하세요"
+            name="phone"
           />
         </div>
-        <div className="flex items-center justify-between mb-[60px]">
+        <div className="flex items-start justify-between lg:mb-[4.0625rem]">
           <div className="flex items-center">
-            <label className="block text-black text-base" htmlFor="address">
+            <label className="block text-black lg:text-base" htmlFor="address">
               주소
             </label>
-            <span className="text-dark-red text-sm ml-1">*</span>
+            <span className="text-dark-red lg:text-sm lg:ml-1">*</span>
           </div>
-          <input
-            type="text"
-            id="name"
-            autoComplete="name"
-            placeholder="이름을 입력하세요"
-            className="w-[330px] px-3 py-2 text-base border rounded-lg focus:outline-none "
-            name="name"
-          />
+
+          <div className="flex flex-col lg:gap-[0.625rem] lg:w-[20.625rem]">
+            <div className="flex lg:gap-[0.625rem] items-center">
+              <Input
+                width="xs"
+                type="text"
+                id="postcode"
+                placeholder="우편번호"
+                name="postcode"
+              />
+              <button className="lg:w-[4rem] lg:h-[1.875rem] lg:border-[0.0938rem] border-light-gray lg:rounded-[0.3125rem] lg:text-xs text-light-gray lg:hover:border-[.125rem]">
+                우편번호
+              </button>
+            </div>
+            <Input
+              width="md"
+              type="text"
+              id="address-detail1"
+              placeholder="상세주소를 입력하세요"
+              name="address-detail1"
+            />
+            <Input
+              width="md"
+              type="text"
+              id="address-detail2"
+              name="address-detail2"
+            />
+          </div>
         </div>
 
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center lg:mb-[6.25rem]">
           <Button size="xxl">가입하기</Button>
         </div>
       </form>
