1366a5bd42c88d37490c34abdcb3967a63c86a84
imnotpossib1e <habit2me@gmail.com>
2025-07-16 13:07:27 +0900

    💄 style: 찜 상품 UI

--- src/app/mypage/likes/page.tsx
+++ src/app/mypage/likes/page.tsx
@@ -1,9 +1,18 @@
 // import Link from 'next/link';
 
+import LikeList from '@/app/mypage/likes/LikeList';
+
 export default async function Likes() {
   return (
-    <div className="lg:w-[49.875rem] flex flex-col lg:gap-2">
-      <h3 className="lg:w-full lg:text-xl font-semibold">내가 찜한 상품</h3>
-    </div>
+    <main className="flex flex-col gap-4">
+      <div className="lg:w-[49.875rem] flex flex-col lg:gap-2">
+        <h3 className="lg:w-full lg:text-xl font-semibold">내가 찜한 상품</h3>
+      </div>
+      <div className="flex flex-col gap-2.5">
+        <LikeList />
+        <LikeList />
+        <LikeList />
+      </div>
+    </main>
   );
 }
