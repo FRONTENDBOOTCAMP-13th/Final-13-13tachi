7e43f909c3cbe2ae7eb8bb11fc61fd679d0f293d
Kanghyeon00 <hyeony0419@naver.com>
2025-07-16 13:38:57 +0900

    ✨ feat: 레시피 검색결과 페이지 마크업 및 스타일링 완료

---
+++ src/app/recipe/search/RecipeSearch.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import Image from 'next/image';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function RecipeSearchInput() {
+  const [text, setText] = useState('');
+  const router = useRouter();
+
+  const handleSearch = () => {
+    if (text.trim() !== '') {
+      router.push(`/recipe/search/${encodeURIComponent(text.trim())}`);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className="flex items-center lg:w-[285px] lg:h-[40px] border border-dark-green rounded-3xl overflow-hidden">
+      <input
+        type="text"
+        placeholder="상품명을 입력해주세요"
+        value={text}
+        onChange={e => setText(e.target.value)}
+        onKeyDown={handleKeyDown}
+        className="flex-grow px-3 text-sm outline-none"
+      />
+      <button
+        type="button"
+        onClick={handleSearch}
+        className="w-10 h-full flex justify-center items-center"
+      >
+        <Image src="/search.svg" alt="검색" width={16} height={16} />
+      </button>
+    </div>
+  );
+}
