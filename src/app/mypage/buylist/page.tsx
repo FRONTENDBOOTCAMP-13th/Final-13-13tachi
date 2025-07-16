b4fdebcd357a92c2740313314359099a3dd0abe1
imnotpossib1e <habit2me@gmail.com>
2025-07-16 13:06:25 +0900

    💄 style: 구매 목록 UI

--- src/app/mypage/buylist/page.tsx
+++ src/app/mypage/buylist/page.tsx
@@ -1,12 +1,15 @@
 // import Link from 'next/link';
 
+import BuyList from '@/app/mypage/buylist/BuyList';
+
 export default async function buyList() {
   return (
-    <>
-      <div className="lg:w-[49.875rem] flex flex-col lg:gap-2">
+    <main className="flex flex-col">
+      <div className="lg:w-[49.875rem] flex flex-col lg:gap-2 mb-[1.875rem]">
         <h3 className="lg:w-full lg:text-xl font-semibold">주문내역</h3>
-        <div className="border-b border-light-gray"></div>
+        <hr className="text-light-gray w-full" />
       </div>
-    </>
+      <BuyList />
+    </main>
   );
 }
