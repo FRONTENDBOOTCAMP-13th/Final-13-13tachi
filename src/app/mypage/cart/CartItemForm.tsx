7938ab80db8f77ac2020c582046ff97a03569bdd
imnotpossib1e <habit2me@gmail.com>
2025-07-22 11:29:35 +0900

    ✨ feat: 장바구니 수량 / 삭제

---
+++ src/app/mypage/cart/CartItemForm.tsx
@@ -0,0 +1,119 @@
+'use client';
+import Image from 'next/image';
+
+// 임시 이미지 불러오기
+import profilePic from '../../../images/profile.jpg';
+import Button from '@/components/common/Button';
+import Checkbox from '@/components/common/Checkbox';
+import { CartListProps } from '@/types';
+import { useActionState } from 'react';
+import { deleteCart, updateCartQuantity } from '@/data/actions/cart';
+import useUserStore from '@/zustand/useStore';
+
+export default function CartItemForm({ item }: { item: CartListProps }) {
+  const { user } = useUserStore();
+  const [deleteState, deleteAction, isDeleting] = useActionState(
+    deleteCart,
+    null,
+  );
+  console.log(deleteState, isDeleting);
+  const [quantityState, quantityAction, isUpdating] = useActionState(
+    updateCartQuantity,
+    null,
+  );
+  console.log(quantityState, isUpdating);
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex justify-between lg:w-[49.375rem] lg:my-[30px]">
+          <div className="flex flex-row items-center gap-[1.5625rem]">
+            <label
+              htmlFor={`inputCheckBox-${item._id}`}
+              className="sr-only"
+            ></label>
+            <Checkbox id={`inputCheckBox-${item._id}`} />
+            <div className="flex flex-row lg:gap-3.5 lg:h-[6.25rem]">
+              <Image
+                src={profilePic}
+                alt="상품이미지"
+                width={100}
+                height={100}
+                className="lg:w-[6.25rem] lg:h-[6.25rem] object-cover rounded-lg shadow-image"
+              ></Image>
+              <div className="flex flex-col justify-between">
+                <div>
+                  <>
+                    <span className="lg:text-base font-semibold text-dark-green mr-0.5">
+                      {item.name}
+                    </span>
+                    <span className="lg:text-sm">(350g)</span>
+                  </>
+                  <p className="lg:text-sm mt-1">{item.price}</p>
+                </div>
+                <div className="flex flex-row justify-center items-center gap-5 border-[0.0625rem] rounded-lg lg:w-20 lg:h-[1.875rem] p-1">
+                  <form>
+                    <input
+                      type="hidden"
+                      name="accessToken"
+                      value={user?.token?.accessToken ?? ''}
+                    />
+                    <input type="hidden" name="_id" value={item._id} />
+                    <input
+                      type="hidden"
+                      name="quantity"
+                      value={Number(item.quantity) - 1}
+                    />
+                    <button
+                      className="lg:text-base font-semibold hover:cursor-pointer"
+                      formAction={quantityAction}
+                    >
+                      -
+                    </button>
+                  </form>
+                  <span className="lg:text-sm">{item.quantity}</span>
+                  <form>
+                    <input
+                      type="hidden"
+                      name="accessToken"
+                      value={user?.token?.accessToken ?? ''}
+                    />
+                    <input type="hidden" name="_id" value={item._id} />
+                    <input
+                      type="hidden"
+                      name="quantity"
+                      value={Number(item.quantity) + 1}
+                    />
+                    <button
+                      className="lg:text-base font-semibold hover:cursor-pointer"
+                      formAction={quantityAction}
+                    >
+                      +
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-end gap-[1.5625rem]">
+            <form>
+              <input
+                type="hidden"
+                name="accessToken"
+                value={user?.token?.accessToken ?? ''}
+              />
+              <input type="hidden" name="_id" value={item._id} />
+              <Button size="sm" variant="white" formAction={deleteAction}>
+                삭제
+              </Button>
+            </form>
+            <span className="lg:text-base font-semibold">
+              {item.price * item.quantity}
+            </span>
+          </div>
+        </div>
+        <hr className="text-light-gray w-full" />
+      </div>
+    </>
+  );
+}
---
+++ src/data/actions/cart.ts
@@ -0,0 +1,101 @@
+'use server';
+import { ApiRes, ApiResPromise, CartItemType } from '@/types';
+import { revalidateTag } from 'next/cache';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
+
+/**
+ * 장바구니 개별 삭제
+ * @param {ApiRes<PostReply> | null} state - 이전 상태(사용하지 않음)
+ * @param {FormData} formData - 삭제할 댓글 정보를 담은 FormData 객체
+ * @returns {Promise<ApiRes<PostReply>>} - 삭제 결과 응답 객체
+ * @description
+ * 댓글을 삭제하고, 성공 시 해당 게시글의 댓글 목록을 갱신합니다.
+ */
+export async function deleteCart(
+  state: ApiRes<CartItemType> | null,
+  formData: FormData,
+): ApiResPromise<CartItemType> {
+  const _id = formData.get('_id');
+  const accessToken = formData.get('accessToken');
+
+  let res: Response;
+  let data: ApiRes<CartItemType>;
+
+  try {
+    res = await fetch(`${API_URL}/carts/${_id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Client-Id': CLIENT_ID,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    data = await res.json();
+  } catch (error) {
+    // 네트워크 오류 처리
+    console.error(error);
+    return { ok: 0, message: '일시적인 네트워크 문제가 발생했습니다.' };
+  }
+
+  if (data.ok) {
+    revalidateTag(`carts/${_id}`);
+  }
+
+  return data;
+}
+
+/**
+ * 장바구니 수량 수정
+ * @param {ApiRes<Post> | null} state - 이전 상태(사용하지 않음)
+ * @param {FormData} formData - 게시글 정보를 담은 FormData 객체
+ * @returns {Promise<ApiRes<Post>>} - 수정 결과 응답 객체
+ * @description
+ * 게시글을 수정하고, 성공 시 해당 게시글 상세 페이지로 이동합니다.
+ * 실패 시 에러 메시지를 반환합니다.
+ */
+export async function updateCartQuantity(
+  state: ApiRes<CartItemType> | null,
+  formData: FormData,
+): ApiResPromise<CartItemType> {
+  const _id = formData.get('_id');
+  const accessToken = formData.get('accessToken'); // 인증 토큰
+
+  const body = {
+    quantity: formData.get('quantity'),
+  };
+
+  let res: Response;
+  let data: ApiRes<CartItemType>;
+
+  try {
+    // 게시글 수정 API 호출
+    res = await fetch(`${API_URL}/carts/${_id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Client-Id': CLIENT_ID,
+        Authorization: `Bearer ${accessToken}`, // 인증 토큰
+      },
+      body: JSON.stringify(body),
+    });
+
+    data = await res.json();
+  } catch (error) {
+    // 네트워크 오류 처리
+    console.error(error);
+    return { ok: 0, message: '일시적인 네트워크 문제가 발생했습니다.' };
+  }
+
+  // 수정 성공 시 해당 게시글 상세 페이지로 이동
+  if (data.ok) {
+    revalidateTag(`carts/${_id}`); // 게시글 상세 페이지 갱신
+    revalidateTag(`carts/`);
+  } else {
+    return data;
+  }
+
+  return data;
+}
