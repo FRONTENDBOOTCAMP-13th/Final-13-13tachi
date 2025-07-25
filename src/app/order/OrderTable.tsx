335cee990b9244fd3d5e625083c9771b8fcae361
Github <khmin1106@gmail.com>
2025-07-26 00:53:31 +0900

    💄 style: 테이블 텍스트 스타일 변경

--- src/app/order/OrderTable.tsx
+++ src/app/order/OrderTable.tsx
@@ -24,10 +24,14 @@ export default function OrderTable() {
     <table className="w-full text-center border-collapse table-fixed mb-10">
       <thead>
         <tr>
-          <th className="border border-l-0 py-3 bg-disable-gray">주문 금액</th>
-          <th className="border py-3 bg-disable-gray">적립금 사용</th>
-          <th className="border py-3 bg-disable-gray">배송비</th>
-          <th className="border border-r-0 py-3 bg-disable-gray">
+          <th className="border border-l-0 py-3 bg-disable-gray font-normal">
+            주문 금액
+          </th>
+          <th className="border py-3 bg-disable-gray font-normal">
+            적립금 사용
+          </th>
+          <th className="border py-3 bg-disable-gray font-normal">배송비</th>
+          <th className="border border-r-0 py-3 bg-disable-gray font-normal">
             결제 예상 금액
           </th>
         </tr>
@@ -39,15 +43,17 @@ export default function OrderTable() {
             <PlusCircle className="text-black fill-white absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2" />
           </td>
           <td className="border relative py-4">
-            <span>0</span>
+            <span className="font-semibold">0</span>
             <MinusCircle className="text-black fill-white absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2" />
           </td>
           <td className="border relative py-4">
-            <span>무료배송</span>
+            <span className="font-semibold">무료배송</span>
             <CircleEqualIcon className="text-black fill-white absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2" />
           </td>
           <td className="border border-r-0 relative py-4">
-            <span>{(res.cost?.total ?? 0).toLocaleString()}</span>
+            <span className="font-semibold text-dark-red">
+              {(res.cost?.total ?? 0).toLocaleString()}
+            </span>
           </td>
         </tr>
       </tbody>
