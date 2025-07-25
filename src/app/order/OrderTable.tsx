2e59aeecb4d104190793fcf811ce260e103654ab
Github <khmin1106@gmail.com>
2025-07-25 16:25:24 +0900

    ✨ feat: 결제금액 테이블 데이터 연결

--- src/app/order/OrderTable.tsx
+++ src/app/order/OrderTable.tsx
@@ -1,6 +1,25 @@
+'use client';
+
+import { getCartProducts } from '@/data/functions/post';
+import { ApiResCart, CartItemType } from '@/types';
+import useUserStore from '@/zustand/useStore';
 import { CircleEqualIcon, MinusCircle, PlusCircle } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 export default function OrderTable() {
+  const { user } = useUserStore();
+  const accessToken = user?.token?.accessToken;
+  const [res, setRes] = useState<ApiResCart<CartItemType[]> | null>(null);
+
+  useEffect(() => {
+    if (accessToken) {
+      getCartProducts(accessToken).then(setRes);
+    }
+  }, [accessToken]);
+
+  if (!res) return <div>로딩중...</div>;
+  if (res.ok === 0) return <div>{res.message}</div>;
+
   return (
     <table className="w-full text-center border-collapse table-fixed mb-10">
       <thead>
@@ -16,19 +35,19 @@ export default function OrderTable() {
       <tbody>
         <tr>
           <td className="border border-l-0 relative py-4">
-            <span>10000</span>
+            <span>{(res.cost?.total ?? 0).toLocaleString()}</span>
             <PlusCircle className="text-black fill-white absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2" />
           </td>
           <td className="border relative py-4">
-            <span>10000</span>
+            <span>0</span>
             <MinusCircle className="text-black fill-white absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2" />
           </td>
           <td className="border relative py-4">
-            <span>10000</span>
+            <span>무료배송</span>
             <CircleEqualIcon className="text-black fill-white absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2" />
           </td>
           <td className="border border-r-0 relative py-4">
-            <span>10000</span>
+            <span>{(res.cost?.total ?? 0).toLocaleString()}</span>
           </td>
         </tr>
       </tbody>
