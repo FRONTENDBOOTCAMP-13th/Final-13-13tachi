175284115ebb2786db2cf1a76929a84762d38d4e
imnotpossib1e <habit2me@gmail.com>
2025-07-26 18:30:56 +0900

    💄 style: 장바구니 반응형 UI

--- src/app/mypage/cart/page.tsx
+++ src/app/mypage/cart/page.tsx
@@ -3,9 +3,9 @@ import CartList from '@/app/mypage/cart/CartList';
 
 export default async function Cart() {
   return (
-    <main className="flex flex-col h-full">
-      <div className="lg:w-[49.875rem] flex flex-col lg:gap-2">
-        <h3 className="lg:w-full lg:text-xl font-semibold">장바구니</h3>
+    <main className="flex flex-col lg:w-[49.875rem] md:w-[31.75rem] w-80 h-full">
+      <div className="flex flex-col gap-2">
+        <h3 className="w-full text-xl font-semibold">장바구니</h3>
         <hr className="text-light-gray w-full" />
       </div>
       <CartList />
