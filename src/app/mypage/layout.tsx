e472b6c28f73120f4119c1b991735c71c9a1d884
imnotpossib1e <habit2me@gmail.com>
2025-07-26 18:31:11 +0900

    💄 style: 마이페이지 레이아웃 반응형 UI 수정

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -40,8 +40,8 @@ export default function RootLayout({
   return (
     <>
       <Header />
-      <div className="flex justify-center mt-[4.0625rem] mb-[6.25rem]">
-        <div className="flex flex-row lg:max-w-5xl md:max-w-[44.25rem]">
+      <div className="flex justify-center  mt-[4.0625rem] mb-[6.25rem] md:p-[1.125rem]">
+        <div className="flex flex-row lg:w-5xl md:w-[44.25rem] w-80">
           <div className="flex flex-col gap-[2.1875rem]">
             <h2 className="text-sm text-gray">
               <Link href="/">HOME</Link>
