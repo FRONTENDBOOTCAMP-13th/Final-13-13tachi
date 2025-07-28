4af9ffafde11ec5bb02529d6b2d77041c42676fc
onewayay <onewayay@naver.com>
2025-07-28 11:34:31 +0900

    🐛 fix: 찜하기 데이터 속성 옵셔널로 수정

--- src/components/ProductCardItem.tsx
+++ src/components/ProductCardItem.tsx
@@ -30,7 +30,7 @@ export default function ProductCardItem({
     likeRes &&
     Object.entries(likeRes)
       .filter(([key]) => key !== 'ok')
-      .some(([, value]) => value.product._id == item._id);
+      .some(([, value]) => value?.product?._id == item._id);
 
   useEffect(() => {
     //찜하기 데이터에 있는지 여부를 isLike/setIsLike로 상태관리
