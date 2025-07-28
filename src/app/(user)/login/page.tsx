531c3a8274dab74dcae7a1ec2efddd4f6311d267
Github <khmin1106@gmail.com>
2025-07-28 14:02:30 +0900

    💄 style: 로그인 UI 반응형

--- src/app/(user)/login/page.tsx
+++ src/app/(user)/login/page.tsx
@@ -1,19 +1,5 @@
 import LoginForm from '@/app/(user)/login/LoginForm';
-import { Metadata } from 'next';
 import Image from 'next/image';
-
-export const metadata: Metadata = {
-  title: `로그인 - UgVeg : 흙내음 상점`,
-  description: `흙내음 상점에서 자연의 맛을 로그인하세요`,
-  openGraph: {
-    title: `로그인 - UgVeg : 흙내음 상점`,
-    description: `흙내음 상점에서 자연의 맛을 로그인하세요`,
-    url: `/login`,
-    images: {
-      url: '/thumbnail.svg',
-    },
-  },
-};
 
 export default async function LoginPage() {
   return (
@@ -24,14 +10,21 @@ export default async function LoginPage() {
       />
       <div className="absolute inset-0 -z-10 bg-black opacity-50" />
 
-      <main className="lg:py-[5.9375rem] flex justify-center items-center min-h-full">
+      <main className=" py-[110px] lg:py-[5.9375rem] flex justify-center items-center min-h-full">
         <div
-          className="lg:py-[4.3125rem] rounded-[0.9375rem] lg:w-[25rem] lg:h-[31.25rem] max-w-md bg-white"
+          className="pt-[30px] lg:py-[4.3125rem] rounded-[0.9375rem] w-[300px] h-[350px] md:w-[25rem] md:h-[31.25rem] lg:w-[25rem] lg:h-[31.25rem] max-w-md bg-white"
           style={{ boxShadow: '.375rem .375rem .25rem rgba(0, 0, 0, 0.25)' }}
         >
           <div className="flex flex-col items-center text-center ">
-            <Image src="/logo-black.svg" alt="로고" width={110} height={70} />
-            <h2 className="lg:text-4xl lg:font-semibold lg:mt-[0.625rem] text-dark-green">
+            <div className="relative w-[95px] h-[50px] md:w-[9rem] md:h-[4.5rem] lg:w-[9rem] lg:h-[4.5rem] mb-2">
+              <Image
+                src="/logo-black.svg"
+                alt="로고"
+                fill
+                className="object-contain"
+              />
+            </div>
+            <h2 className="text-xl md:text-4xl lg:text-4xl font-semibold  lg:mt-[0.625rem] text-dark-green">
               로그인
             </h2>
           </div>
--- src/app/(user)/login/select/page.tsx
+++ src/app/(user)/login/select/page.tsx
@@ -1,38 +1,24 @@
-import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export const metadata: Metadata = {
-  title: `로그인 - UgVeg : 흙내음 상점`,
-  description: `흙내음 상점에서 자연의 맛을 로그인하세요`,
-  openGraph: {
-    title: `로그인 - UgVeg : 흙내음 상점`,
-    description: `흙내음 상점에서 자연의 맛을 로그인하세요`,
-    url: `/login/select`,
-    images: {
-      url: '/thumbnail.svg',
-    },
-  },
-};
-
 export default async function LoginPage() {
   return (
-    <div className="relative flex items-center justify-center lg:min-h-[calc(100dvh-21.625rem)]">
+    <div className="relative flex items-center justify-center min-h-[calc(100dvh-25rem)] md:min-h-[calc(100dvh-20.185rem)] lg:min-h-[calc(100dvh-21.625rem)]">
       <div
         className="absolute inset-0 -z-10 bg-cover bg-center"
         style={{ backgroundImage: "url('/login-img.svg')" }}
       />
       <div className="absolute inset-0 -z-10 bg-black opacity-50" />
       <div className="flex flex-col items-center ">
-        <h2 className="lg:text-xl lg:font-bold text-center text-white lg:mb-[1.25rem]">
+        <h2 className="text-base font-semibold md:text-lg md:font-semibold lg:text-xl lg:font-bold text-center text-white mb-[1.25rem]">
           흙내음 상점에서 <br /> 자연의 맛을 로그인하세요
         </h2>
 
         {/* 이메일 로그인 */}
-        <div className="flex flex-col lg:gap-[0.625rem] w-full relative">
+        <div className="flex flex-col gap-[0.625rem] w-full relative">
           <Link
             href="/login"
-            className="flex items-center justify-center lg:w-[20.3125rem] lg:h-[3.4375rem] border-white border-[0.1875rem] lg:text-base lg:font-semibold text-white text-center rounded-lg hover:bg-black/15"
+            className="flex items-center justify-center w-[280px] h-[45px] md:w-[264px] md:h-[45px] lg:w-[20.3125rem] lg:h-[3.4375rem] border-white border-[0.1875rem] text-sm lg:text-base lg:font-semibold text-white text-center rounded-lg hover:bg-black/15"
           >
             이메일로 시작하기
           </Link>
@@ -40,7 +26,7 @@ export default async function LoginPage() {
           {/* 카카오 로그인 */}
           <Link
             href="/api/auth/kakao"
-            className="block lg:w-[20.3125rem] lg:h-[3.4375rem]"
+            className="block text-sm w-[280px] h-[45px] md:w-[264px] md:h-[45px] lg:w-[20.3125rem] lg:h-[3.4375rem]"
           >
             <Image
               src="/kakao.svg"
@@ -54,7 +40,7 @@ export default async function LoginPage() {
           {/* 네이버 로그인 */}
           <Link
             href="/api/auth/naver"
-            className="block lg:w-[20.3125rem] lg:h-[3.4375rem]"
+            className="block text-sm w-[280px] h-[45px] md:w-[264px] md:h-[45px] lg:w-[20.3125rem] lg:h-[3.4375rem]"
           >
             <Image
               src="/naver.svg"
@@ -68,7 +54,7 @@ export default async function LoginPage() {
           {/* 회원가입 */}
           <Link
             href="/signup"
-            className="absolute right-0 lg:bottom-[-2rem] lg:text-sm text-white hover:underline"
+            className="absolute right-0 bottom-[-2rem] text-sm text-white hover:underline"
           >
             회원가입
           </Link>
