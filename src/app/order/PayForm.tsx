2675090163f91fdc23ab5cb4fb2e3f74a0054811
Github <khmin1106@gmail.com>
2025-07-30 22:45:16 +0900

    💄 style: 무통장입금 초기 선택 필수

--- src/app/order/PayForm.tsx
+++ src/app/order/PayForm.tsx
@@ -8,7 +8,7 @@ import tosspay from '../../images/tosspay.png';
 import { useState } from 'react';
 
 export default function PayForm() {
-  const [payment, setPayment] = useState('내 통장 결제');
+  const [payment, setPayment] = useState('무통장 입금');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
