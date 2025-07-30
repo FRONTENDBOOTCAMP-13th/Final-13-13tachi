b94e1d6246e20b2c2701d77cfff73309fad4fa7b
imnotpossib1e <habit2me@gmail.com>
2025-07-31 00:14:24 +0900

    💄 style: 메일 ui 수정

--- src/data/actions/cart.ts
+++ src/data/actions/cart.ts
@@ -338,35 +338,110 @@ export async function sendEmail(
   let totalPrice = 0;
   products.map(p => (totalPrice += p.price));
 
+  // const content = `
+  // <div style="margin:0 auto;max-width:600px; font-family: Arial, sans-serif; color:#333;">
+  //   <h2>${sellerName} 농부님! 새 주문이 접수되었습니다!</h2>
+  //   <p><b>주문 번호:</b> ${orderNum}</p>
+  //   <p><b>구매자 이름:</b> ${user.name}님</p>
+  //   <p><b>연락처:</b> ${user.phone}</p>
+  //   <p><b>배송지 주소:</b><br>
+  //      ${user.addressDetail1} ${user.addressDetail2} (${user.postcode})
+  //   </p>
+  //   ${user.message ? `<p><b>배송 요청사항:</b> ${user.message}</p>` : ''}
+  //   <h3>주문 상품 내역</h3>
+  //   <ul>
+  //     ${products
+  //       .map(
+  //         product => `
+  //       <li style="margin-bottom: 10px;">
+  //         <b>${product.name}</b> (${product.extra?.details})<br>
+  //         수량: ${product.quantity}개 / 가격: ${product.price.toLocaleString()}원<br>
+  //         카테고리: ${product.extra?.category?.join(', ')}<br>
+  //       </li>
+  //     `,
+  //       )
+  //       .join('')}
+  //   </ul>
+  //   <hr>
+  //   <p><b>총 결제 금액:</b> ${totalPrice.toLocaleString()}원</p>
+  //   <p>빠른 배송 부탁드립니다.</p>
+  // </div>
+  // `;
+
   const content = `
-  <div style="margin:0 auto;max-width:600px; font-family: Arial, sans-serif; color:#333;">
-    <h2>${sellerName} 농부님! 새 주문이 접수되었습니다!</h2>
-    <p><b>주문 번호:</b> ${orderNum}</p>
-    <p><b>구매자 이름:</b> ${user.name}님</p>
-    <p><b>연락처:</b> ${user.phone}</p>
-    <p><b>배송지 주소:</b><br>
-       ${user.addressDetail1} ${user.addressDetail2} (${user.postcode})
+  <div style="margin:0 auto; max-width:600px; font-family: Arial, sans-serif; ">
+    <h1 style="font-size: xx-large; font-weight: 700; color: darkgreen; margin-bottom: 50px; text-align: center;">
+      UgVeg: 흙내음 상점
+    </h1>
+    <h2 style="margin-bottom: 40px;">${sellerName} 농부님! 새 주문이 접수되었습니다!</h2>
+    <p style="font-size: large; font-weight: 700; margin-bottom: 14px;">배송지 정보</p>
+    <div style="display: flex; flex-direction: column; gap: 10px; border-radius: 20px; background-color: rgb(240, 240, 240); padding: 20px; margin-bottom: 40px;">
+      <div style="display: flex;">
+        <div style="display: inline-block; width: 115px;"><b>주문 번호</b></div>
+        <span style="display: inline-block; width: 40px;">|</span>
+        <span>${orderNum}</span>
+      </div>
+      <div style="display: flex;">
+        <div style="display: inline-block; width: 115px;"><b>받으시는 분</b></div>
+        <span style="display: inline-block; width: 40px;">|</span>
+        <span>${user.name}</span>
+      </div>
+      <div style="display: flex;">
+        <div style="display: inline-block; width: 115px;"><b>전화번호</b></div>
+        <span style="display: inline-block; width: 40px;">|</span>
+        <span>${user.phone}</span>
+      </div>
+      <div style="display: flex;">
+        <div style="display: inline-block; width: 115px; flex-shrink: 0;"><b>배송지 주소</b></div>
+        <span style="display: inline-block; width: 40px; flex-shrink: 0;">|</span>
+        <div style="display: block;">
+          <span>${user.addressDetail1}</span>&nbsp;<span>${user.addressDetail2}</span>&nbsp;<span>(${user.postcode})</span>
+        </div>
+      </div>
+      ${
+        user.message
+          ? ` 
+            <div style="display: flex;">
+              <span style="display: inline-block; width: 115px; flex-shrink: 0;"><b>배송 요청사항</b></span>
+              <span style="display: inline-block; width: 40px; flex-shrink: 0;">|</span>
+              <span>${user.message}</span>
+            </div>`
+          : ''
+      }
+    </div>
+    <p style="font-size: large; font-weight: 700; margin-bottom: 14px;">구매 정보</p>
+    <div style="display: flex; flex-direction: column; gap: 20px; margin-bottom: 40px;">
+      ${products.map(
+        product => `
+          <div style="display: flex; flex-direction: column; gap: 10px; border-radius: 20px; background-color: rgb(240, 240, 240); padding: 20px;">
+            <div style="display: flex; flex-direction: column; gap: 10px;">
+              <div style="display: flex;">
+                <div style="display: inline-block; width: 115px;"><b>상품명</b></div>
+                <span style="display: inline-block; width: 40px;">|</span>
+                <div style="display: flex; align-items: baseline;"><span>${product.name}</span><span style="font-size: small; margin-left: 10px;">(${product.extra?.details})</span></div>
+              </div>
+              <div style="display: flex;">
+                <div style="display: inline-block; width: 115px;"><b>주문 수량</b></div>
+                <span style="display: inline-block; width: 40px;">|</span>
+                <span>${product.quantity}개</span>
+              </div>
+              <div style="display: flex;">
+                <div style="display: inline-block; width: 115px;"><b>가격</b></div>
+                <span style="display: inline-block; width: 40px;">|</span>
+                <span>${product.price.toLocaleString()}원</span>
+              </div>
+            </div>
+          </div>
+        `,
+      )}
+    </div>
+    <hr style="margin-bottom: 40px;"/>
+    <p style="font-size:large; text-align: right;">
+      <b>총 결제금액 <span style="font-size: x-large; margin-left: 20px;">${totalPrice.toLocaleString()}원</span> </b>
     </p>
-    ${user.message ? `<p><b>배송 요청사항:</b> ${user.message}</p>` : ''}
-    <h3>주문 상품 내역</h3>
-    <ul>
-      ${products
-        .map(
-          product => `
-        <li style="margin-bottom: 10px;">
-          <b>${product.name}</b> (${product.extra?.details})<br>
-          수량: ${product.quantity}개 / 가격: ${product.price.toLocaleString()}원<br>
-          카테고리: ${product.extra?.category?.join(', ')}<br>
-        </li>
-      `,
-        )
-        .join('')}
-    </ul>
-    <hr>
-    <p><b>총 결제 금액:</b> ${totalPrice.toLocaleString()}원</p>
-    <p>빠른 배송 부탁드립니다.</p>
+    <p style="margin-top: 40px; text-align: center;">소중한 농작물이 신선하게 잘 도착할 수 있도록 안전하고 신속한 배송 부탁드립니다. 감사합니다.</p>
   </div>
-  `;
+`;
 
   const body = {
     to: sellerEmail,
