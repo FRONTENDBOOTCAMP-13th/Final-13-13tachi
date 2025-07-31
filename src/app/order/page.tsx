d51db372593b06a2fc9b0f83c6b0628f060633ad
Github <khmin1106@gmail.com>
2025-07-31 17:09:28 +0900

    🐛 bug: build 에러 Suspense

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,9 +1,12 @@
 import OrderForm from '@/app/order/OrderForm';
+import { Suspense } from 'react';
 
 export default function Order() {
   return (
-    <main className="min-h-[calc(100dvh-23.625rem)] md:min-h-[calc(100dvh-20.1875rem)] lg:min-h-[calc(100dvh-21.625rem)]">
-      <OrderForm />
-    </main>
+    <Suspense>
+      <main className="min-h-[calc(100dvh-23.625rem)] md:min-h-[calc(100dvh-20.1875rem)] lg:min-h-[calc(100dvh-21.625rem)]">
+        <OrderForm />
+      </main>
+    </Suspense>
   );
 }
